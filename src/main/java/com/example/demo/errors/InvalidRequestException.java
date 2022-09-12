a8e9c5bf6f7f2d98188e5ff238f14b95fdf928d0
Mit Pandya <88765741+Mit076er@users.noreply.github.com>
2022-09-12 13:15:18 +0530

    Update InvalidRequestException.java

--- src/main/java/com/example/demo/errors/InvalidRequestException.java
+++ src/main/java/com/example/demo/errors/InvalidRequestException.java
@@ -1,11 +1,16 @@
 package com.example.demo.errors;
 
 import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
 @ResponseStatus(HttpStatus.BAD_REQUEST)
+
 public class InvalidRequestException extends RuntimeException {
-    public InvalidRequestException(String s) {
+    public InvalidRequestException(String s) 
+    
+    {
         super(s);
     }
 }
