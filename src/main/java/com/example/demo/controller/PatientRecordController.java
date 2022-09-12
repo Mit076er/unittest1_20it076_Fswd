1498ad17352ddd660ced735acb90daf2282d476a
Mit Pandya <88765741+Mit076er@users.noreply.github.com>
2022-09-12 13:14:35 +0530

    Update PatientRecordController.java

--- src/main/java/com/example/demo/controller/PatientRecordController.java
+++ src/main/java/com/example/demo/controller/PatientRecordController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+
 import com.example.demo.model.PatientRecord;
+
 import com.example.demo.repositories.PatientRecordRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,27 +22,25 @@ public class PatientRecordController {
         return patientRecordRepository.findAll();
     }
 
-    // Get the student information
     @GetMapping("/patient/{id}")
     public PatientRecord getPatient(@PathVariable Long id) {
         return patientRecordRepository.findById(id).get();
     }
 
-    // Delete the student
     @DeleteMapping("/patient/{id}")
     public List<PatientRecord> deleteStudent(@PathVariable Long id) {
         patientRecordRepository.delete(patientRecordRepository.findById(id).get());
         return patientRecordRepository.findAll();
     }
 
-    // Add new student
+   
     @PostMapping("/patient")
     public List<PatientRecord> addStudent(@RequestBody PatientRecord patientRecord) {
         patientRecordRepository.save(patientRecord);
         return patientRecordRepository.findAll();
     }
 
-    // Update the student information
+    
     @PutMapping("/patient/{id}")
     public List<PatientRecord> updateStudent(@RequestBody PatientRecord patientRecord, @PathVariable Long id) {
         PatientRecord patientObj = patientRecordRepository.findById(id).get();
